0c29cf29a9a7f8ed07f43d53d6434b865b50025d
villno55 <adsotrabajos24@gmail.com>
2025-09-06 18:20:45 -0500

    Add console

---
+++ ejercicio6/main.js
@@ -0,0 +1,6 @@
+import { esElegibleParaPrestamo } from "./utilidades.js";
+
+console.log(esElegibleParaPrestamo(4000000, 700));  
+console.log(esElegibleParaPrestamo(2500000, 700));  
+console.log(esElegibleParaPrestamo(3500000, 600));  
+console.log(esElegibleParaPrestamo(5000000, 800));  
