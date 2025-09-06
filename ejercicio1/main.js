e5ec152baed4100a513d4550b196e047db938a5f
villno55 <adsotrabajos24@gmail.com>
2025-09-06 12:53:55 -0500

    Update main.js

--- ejercicio1/main.js
+++ ejercicio1/main.js
@@ -2,3 +2,8 @@ import { enRango } from "./enRango.js";
 
 console.log(enRango(25));  
 console.log(enRango(5));   
+console.log(enRango(10));  
+console.log(enRango(50));  
+console.log(enRango(70));  
+
+
