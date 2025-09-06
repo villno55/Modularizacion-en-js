745c4a43031ed76fce29459bbd07009470521c9c
villno55 <adsotrabajos24@gmail.com>
2025-09-06 11:52:15 -0500

    Add main

---
+++ ejercicio2/main.js
@@ -0,0 +1,9 @@
+import { calcularDescuento } from "./utilidades.js";
+
+console.log(calcularDescuento(1500)); 
+console.log(calcularDescuento(1200)); 
+console.log(calcularDescuento(1000)); 
+console.log(calcularDescuento(750));  
+console.log(calcularDescuento(500));  
+console.log(calcularDescuento(400));  
+console.log(calcularDescuento(100)); 
