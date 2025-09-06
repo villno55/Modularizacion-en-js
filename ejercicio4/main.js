e8e55e4f4a5f0f9f202de4eaa3627537b8cc6450
villno55 <adsotrabajos24@gmail.com>
2025-09-06 18:08:31 -0500

    Update main.js

---
+++ ejercicio4/main.js
@@ -0,0 +1,6 @@
+import { nivRiesgo } from "./utilidades.js";
+
+console.log(nivRiesgo(65, false));   
+console.log(nivRiesgo(50, true));    
+console.log(nivRiesgo(30, false));   
+console.log(nivRiesgo(45, false));
