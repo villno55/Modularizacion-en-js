6bb83d7145f9b850b75bbec58ac1eebc393b1c10
villno55 <adsotrabajos24@gmail.com>
2025-09-06 13:15:37 -0500

    Update main.js

---
+++ ejercicio3/main.js
@@ -0,0 +1,8 @@
+import { categoriaIMC } from "./utilidades.js";
+
+console.log(categoriaIMC(17));    
+console.log(categoriaIMC(22));    
+console.log(categoriaIMC(27));    
+console.log(categoriaIMC(32));    
+console.log(categoriaIMC(18.5));  
+console.log(categoriaIMC(24.9));  
