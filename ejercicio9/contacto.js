f2e7b34de2b6f6447e117152051a573ea2291ce0
villno55 <adsotrabajos24@gmail.com>
2025-09-07 10:09:05 -0500

    Chaged 1

---
+++ ejercicio9/contacto.js
@@ -0,0 +1,9 @@
+export default class Contacto {
+  constructor(identificacion, nombre, apellido, correo, celular) {
+    this.identificacion = identificacion;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.correo = correo;
+    this.celular = celular;
+  }
+}
